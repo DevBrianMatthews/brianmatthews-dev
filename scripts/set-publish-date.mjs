17863bdc63e053278d5c3a514f13a9ef1c177a39
BrianMatthews <brianmatthews.contact@gmail.com>
2026-08-20 22:57:36 -0500

    fix: update publish dates

--- scripts/set-publish-date.mjs
+++ scripts/set-publish-date.mjs
@@ -8,7 +8,7 @@ function getTodayDate() {
     const yyyy = today.getFullYear();
     const mm = String(today.getMonth() + 1).padStart(2, "0");
     const dd = String(today.getDate()).padStart(2, "0");
-    return `${yyyy}-${mm}-${dd}`;
+    return `${yyyy}-${mm}-${dd}T12:00:00`;
 }
 
 function processFile(filePath) {
